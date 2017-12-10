fe1051575b523d21e5a271c65bd121a4cd71e340
Sagar26 <sagar.g@empoweringvisions.com>
2017-12-10 14:46:38 +0530

    1.a char drawable added.

--- app/src/main/java/com/example/rahulk/simplycustomkeyboard/CustomeKeyboardView.java
+++ app/src/main/java/com/example/rahulk/simplycustomkeyboard/CustomeKeyboardView.java
@@ -1,14 +1,9 @@
 package com.example.rahulk.simplycustomkeyboard;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.util.AttributeSet;
-
-import java.util.List;
 
 /**
  * Created by ADMIN on 12/3/2017.
@@ -32,20 +27,20 @@ public class CustomeKeyboardView extends KeyboardView {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        @SuppressLint("DrawAllocation") Paint paint = new Paint();
+       /* @SuppressLint("DrawAllocation") Paint paint = new Paint();
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setTextSize(68);
         List<Keyboard.Key> keys = getKeyboard().getKeys();
         for (Keyboard.Key key : keys) {
 
-            if (key.codes[0]!= -4 && key.codes[0] != 12 && key.codes[0] != 32 && key.codes[0] != 40 && key.codes[0] != -5) {
+            if (key.codes[0] != -4 && key.codes[0] != 12 && key.codes[0] != 32 && key.codes[0] != 40 && key.codes[0] != -5) {
                 if (key.icon != null) {
 
-                    key.icon.setBounds(key.x, key.y + 5, key.x + key.width, key.y + key.height + 3);
+                    key.icon.setBounds(key.x-2, key.y-2, key.width-2,  key.height-2 );
                     key.icon.draw(canvas);
                 }
             }
-
+        }*/
     /*        if (key.codes[0] == 97) {
 
                 Log.e("KEY", "Drawing key with code " + key.codes[0]);
@@ -71,8 +66,8 @@ public class CustomeKeyboardView extends KeyboardView {
                 if (key.label != null) {
                     canvas.drawText(key.label.toString(), key.width / 2, (key.height / 2) + 15, paint);
                 }
-            }*/
+            }
 
-        }
+        }*/
     }
 }
